763fb376d0fa7a9ebf274a826def02a7ccecfe5e
Alim-pro <ialim2299@gmail.com>
2026-05-03 21:51:03 +0500

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "lib.h"
+
 #include <iostream>
 
-int main(int, char**)
-{
+int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "The answer to the Ultimate Question of Life, The Universe, and Everything is: " << answer_to_the_ultimate_question_of_life() << std::endl;
+    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
